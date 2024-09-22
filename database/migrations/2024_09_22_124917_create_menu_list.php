e6336efffb38362b83adb85e5a000a0f483d2d73
zahraalwy2021 <zhakhanza22@gmail.com>
2024-09-22 20:06:24 +0700

    migration
    - add migration menu has role
    - menu list

---
+++ database/migrations/2024_09_22_124917_create_menu_list.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);  
+            $table->string('parent', 50);
+            $table->string('status', 50);  
+            $table->string('number_range');  
+            $table->string('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_list');
+    }
+};
