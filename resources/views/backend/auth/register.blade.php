83625868191cc3ea65c5be246daab3abdb658b4a
abdulmuti12 <abdulmfmuti@gmail.com>
2024-10-20 20:05:09 +0700

    add page regsiter

--- resources/views/backend/auth/register.blade.php
+++ resources/views/backend/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.auth.auth_master')
 
 @section('auth_title')
-    Login | Admin Panel
+    Register | Admin Panel
 @endsection
 
 @section('auth-content')
@@ -52,13 +52,12 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!-- end col -->
 
                                 <div class="col-lg-6">
                                     <div class="p-lg-5 p-4">
                                         <div>
-                                            <h5 class="text-primary">Welcome Back !</h5>
-                                            <p class="text-muted">Sign in to continue to Velzon.</p>
+                                            <h5 class="text-primary">Register!</h5>
+                                            {{-- <p class="text-muted">Sign in to continue to Velzon.</p> --}}
                                         </div>
 
                                         <div class="mt-4">
@@ -75,27 +74,54 @@
                                                 </div>
                                             @endif
 
-                                            <form action="">
-                                            {{-- <form method="POST" action="{{ route('admin.forgotPassword.checkSendMail') }}"> --}}
+                                            <form method="POST" action="">
                                                 @csrf
                                                 <div class="mb-3">
-                                                    <label for="username" class="form-label">Send Mail</label>
+                                                    <label for="username" class="form-label">Email address or Username</label>
                                                     <input type="text" class="form-control" id="username" name="email" placeholder="Enter email or username">
                                                     @error('email')
                                                         <div class="text-danger">{{ $message }}</div>
                                                     @enderror
                                                 </div>
-                                                                                    
-                                                <div class="mt-4">
-                                                    <button class="btn btn-success w-100" type="submit">Send</button>
+                                            
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="password-input">Password</label>
+                                                    <div class="position-relative auth-pass-inputgroup mb-3">
+                                                        <input type="password" class="form-control pe-5 password-input" id="password-input" name="password" placeholder="Enter password">
+                                                        <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                                    </div>
+                                                    @error('password')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
                                                 </div>
                                             
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="1" id="auth-remember-check" name="remember">
+                                                    <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                                </div>
+                                            
+                                                <div class="mt-4">
+                                                    <button class="btn btn-success w-100" type="submit">Register</button>
+                                                </div>
+                                            
+                                                <div class="mt-4 text-center">
+                                                    <div class="signin-other-title">
+                                                        <h5 class="fs-13 mb-4 title">Login Access</h5>
+                                                    </div>
+                                            
+                                                    {{-- <div>
+                                                        <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
+                                                        <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
+                                                        <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
+                                                        <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
+                                                    </div> --}}
+                                                </div>
                                             </form>
                                             
                                         </div>
 
                                         <div class="mt-5 text-center">
-                                            <p class="mb-0"><a href="{{ route('admin.login') }}" class="fw-semibold text-primary text-decoration-underline"> Back To login</a> </p>
+                                            <p class="mb-0">Forget Password? <a href="" class="fw-semibold text-primary text-decoration-underline"> Send Access To Mail</a> </p>
                                         </div>
                                     </div>
                                 </div>
@@ -130,5 +156,4 @@
         </footer>
         <!-- end Footer -->
     </div>
-    <!-- login area end -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthentivicationController; // Make sure the controller is properly imported
 
-Route::get('/', function () {
 
-    dd(333);
-    // return view('welcome');
-});
-
-// Define the route for the login method of AuthentivicationController
-Route::get('login', [AuthentivicationController::class, 'login']); // Use array notation for controller actions in Laravel 8+
+Route::get('/login', [AuthentivicationController::class, 'login'])->name('login'); // Use array notation for controller actions in Laravel 8+
